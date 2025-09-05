b702e178184134e5441be760e92e5f608d8ac10b
SotaTek-TuHoang2 <tuah.ptit@gmail.com>
2025-09-05 18:13:40 +0700

    feat: create tag <p> use to ejs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,8 @@ export class AppController {
 
   @Get()
   @Render('home')
-  getHello() {
-    // return this.appService.getHello();
+  handleHomePage() {
+    const message = this.appService.getHello();
+    return { message };
   }
 }
